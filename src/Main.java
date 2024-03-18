7d493ff1147a4537c250d7097f06f3875b6e4e3b
SabuCay <22Thomas@>
2024-03-18 13:36:20 +0100

    Fint

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+
+        Student mig = new Student("Cay", 34);
+        mig.getName();
+    }
+    public static void MyMethod(){
+        String mm = "Hej";
+    }
+}
